b3f7aff8cbed4c064a7c681bcca93e9ddaa6652a
Piyush <pvvishwa12345@gmail.com>
2025-08-07 21:32:40 +0530

    Peak in Mountain Array

---
+++ FindPeakInMountainArray.java
@@ -0,0 +1,17 @@
+public class FindPeakInMountainArray {
+    public static void main(String[] args) {
+         int arr[] = {0,1,0};
+         int start = 0;
+         int end = arr.length -1;
+         while (start<end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid]<arr[mid + 1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+         }
+         System.out.println(start + " " + end);
+    }
+}
