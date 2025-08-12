c88ea136c1385068afbc83b5fd60a487cc4a83db
Contra-BlackFlag <pvvishwa12345@gmail.com>
2025-08-12 13:04:35 +0530

    Create TryingHardFindInMountainarray.java

---
+++ TryingHardFindInMountainarray.java
@@ -0,0 +1,61 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class TryingHardFindInMountainarray {
+    public static void main(String[] args) {
+
+            int arr[] = {0,5,3,1};
+            int target = 1;
+            int end = FindPeak(arr);
+            int start = end;
+            int ans = binarySearch(arr, target, 0, end,true);
+            int ans1;
+            if (ans == -1) {
+                ans1 = binarySearch(arr,target, start+1, arr.length-1,false);
+                if (ans == -1 && ans1 == -1) {
+                    System.out.println("Not found");
+                }
+                else System.out.println(ans1);
+            }
+            else System.out.println(ans);
+            
+            }
+            public static int binarySearch(int[] arr,int key,int start,int end, boolean Asc){
+            while (start <= end) {
+                    int mid = start + (end - start) / 2;
+                    if (arr[mid] == key) {
+                        return mid;
+                    } else if (arr[mid] > key) {
+                        if(Asc){
+                            end = mid - 1;
+                        }
+                        else end = mid + 1;
+                    } 
+                    else if (arr[mid] < key) {
+                       if(Asc){
+                            start = mid + 1;
+                       }
+                       else start = mid - 1;
+                    }
+                }
+                return -1;
+            }
+        
+        public static int FindPeak(int arr[]){
+         int start = 0;
+         int end = arr.length -1;
+         while (start<end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid]<arr[mid + 1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+         }
+         return end;
+        }
+
+
+    }
+
