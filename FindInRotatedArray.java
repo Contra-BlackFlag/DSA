6061b53784020450320af9e171409c13df6a3768
Piyush Vishwakarma <pvvishwa12345@gmail.com>
2025-08-18 23:41:22 +0530

    Pivots with duplicate elements in array

--- FindInRotatedArray.java
+++ FindInRotatedArray.java
@@ -1,8 +1,8 @@
 public class FindInRotatedArray {
     public static void main(String[] args) {
-        int target = 3;
-        int arr[]= {3,5,1};
-        int pivot = Pivot(arr);
+        int target = 0;
+        int arr[]= {1};
+        int pivot = PivotWithDuplicates(arr);
         if(pivot == -1){
            System.out.println(BinarySearch(0,arr.length - 1,arr,target));
         }
@@ -46,6 +46,35 @@ public class FindInRotatedArray {
                 end = mid -1;
             }
             else start = mid + 1;
+        }
+        return -1;
+    }
+
+    static int PivotWithDuplicates(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start<= end) {
+            int mid = start + (end - start)/2;
+            if (mid<end && arr[mid]>arr[mid + 1]) {
+                return mid;
+            }
+            else if (mid > start && arr[mid]<arr[mid - 1]) {
+                 return mid -1;
+            }
+            if (arr[start] == arr[end] && arr[end] == arr[mid]) {
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end --;
+            }
+            else if (arr[start]<arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            }
+            else end = mid - 1;
         }
         return -1;
     }
---
+++ PivotsDuplicate.java
@@ -0,0 +1,34 @@
+public class PivotsDuplicate {
+    public static void main(String[] args) {
+        int []arr = {1};
+        System.out.println(PivotWithDuplicates(arr));
+    }
+    static int PivotWithDuplicates(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start<= end) {
+            int mid = start + (end - start)/2;
+            if (mid<end && arr[mid]>arr[mid + 1]) {
+                return mid;
+            }
+            else if (mid > start && arr[mid]<arr[mid - 1]) {
+                 return mid -1;
+            }
+            if (arr[start] == arr[end] && arr[end] == arr[mid]) {
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end --;
+            }
+            else if (arr[start]<arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            }
+            else end = mid - 1;
+        }
+        return -1;
+    }
+}
