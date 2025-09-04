5446ee36638bd6f8e902a3253c20b4da00fc7691
Piyush Vishwakarma <pvvishwa12345@gmail.com>
2025-09-05 00:31:16 +0530

    implemented

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -2,20 +2,23 @@ package Sorting;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = {3,1,5,4,2,6,0};
+        int[] arr = {3,1,5,4,2,2,1,4,3,0};
         Sort(arr);
         for (int i : arr) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
     }
     static void Sort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             int max = 0;
             for (int j = 1; j < arr.length - i; j++) {
-                max = Math.max(arr[j],arr[j - 1]);
-                
+                if (arr[j]>arr[max]) {
+                    max = j;
+                }
             }
-            arr[arr.length - i] = max;
+            int temp = arr[arr.length - 1 - i];
+            arr[arr.length - i - 1] = arr[max];
+            arr[max] = temp;
         }
     }
 }
