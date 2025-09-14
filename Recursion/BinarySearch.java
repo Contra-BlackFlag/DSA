028890f902128195d6c739c66fc7933bf070107e
Piyush Vishwakarma <pvvishwa12345@gmail.com>
2025-09-14 21:39:00 +0530

    new xode

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -3,12 +3,15 @@ package Recursion;
 public class BinarySearch {
     public static void main(String[] args) {
         
-        int[] arr = {2,3,4,5,6,7,8};
-        int ans = binarySearchrecursion(arr, 4, 0, arr.length - 1);
+        int[] arr = {-1,0,3,5,9,12};
+        int ans = binarySearchrecursion(arr, 2, 0, arr.length - 1);
         System.out.println(ans);
         
     }
       static int binarySearchrecursion(int[] arr,int key,int start, int end){
+        if (start>end) {
+            return -1;
+        }
         int mid = start + (end - start)/2;
         if (arr[mid] == key) {
             return mid;
